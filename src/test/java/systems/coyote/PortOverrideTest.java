ad37c5f9f732acd207fc2f19af7fa79d148c3d9d
Steve Cote <sdcote@gmail.com>
2017-03-25 17:46:47 -0400

    initial load

---
+++ src/test/java/systems/coyote/PortOverrideTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017 Stephan D. Cote' - All rights reserved.
+ * 
+ * This program and the accompanying materials are made available under the 
+ * terms of the MIT License which accompanies this distribution, and is 
+ * available at http://creativecommons.org/licenses/MIT/
+ *
+ * Contributors:
+ *   Stephan D. Cote 
+ *      - Initial concept and initial implementation
+ */
+package systems.coyote;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * This tests the ability to specify and override the bind port from the 
+ * command line. This is required for operation in Heroku.
+ */
+public class PortOverrideTest {
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @BeforeClass
+  public static void setUpBeforeClass() throws Exception {}
+
+
+
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @AfterClass
+  public static void tearDownAfterClass() throws Exception {}
+
+
+
+
+  @Ignore
+  public void test() {
+    fail( "Not yet implemented" ); // TODO
+  }
+
+}
