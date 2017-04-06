16e131fc5b09286e1e79d8301313dc54063ab78c
Steve Cote <sdcote@gmail.com>
2017-04-06 07:27:18 -0400

    Fleshed out rest of retrieval logic.

--- src/main/java/systems/coyote/handler/LogHandler.java
+++ src/main/java/systems/coyote/handler/LogHandler.java
@@ -11,10 +11,17 @@
  */
 package systems.coyote.handler;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URI;
 import java.util.Enumeration;
 import java.util.Map;
 
 import coyote.commons.StringUtil;
+import coyote.commons.UriUtil;
+import coyote.commons.network.MimeType;
+import coyote.commons.network.http.HTTPD;
 import coyote.commons.network.http.IHTTPSession;
 import coyote.commons.network.http.Response;
 import coyote.commons.network.http.Status;
@@ -23,6 +30,7 @@ import coyote.commons.network.http.handler.UriResource;
 import coyote.commons.network.http.handler.UriResponder;
 import coyote.dataframe.DataFrame;
 import coyote.loader.cfg.Config;
+import coyote.loader.log.Log;
 import coyote.loader.log.LogTool;
 import coyote.loader.log.Logger;
 import systems.coyote.WebServer;
@@ -37,7 +45,6 @@ import systems.coyote.WebServer;
  * Configuration:
  * "/api/log/" : { "Class" : "systems.coyote.handler.LogHandler" },
  * "/api/log/:name" : { "Class" : "systems.coyote.handler.LogHandler" },
- * "/api/log/:name/:type" : { "Class" : "systems.coyote.handler.LogHandler" },
  * 
  * API Overview:
  * get / profile of the logging system
@@ -45,8 +52,7 @@ import systems.coyote.WebServer;
  * 
  * get /name return the contents of that logs target
  * The response will contain a chunk of the log output, as well as the {@code X-Log-Size} header that represents the bytes offset (of the raw log file). This is the number you want to use as the {@code offset} parameter for the next call.
- * get /name/text?offset=0 (raw text of the file)
- * get /name/html?offset=0 (if you want HTML that can be put into <pre> tag.)
+ * get /name?offset=0 (raw text of the file)
  * 
  * Controlling Loggers and Logging by setting attributes:
  * put / change global attributes of the logging system (e.g. log mask) - use GET / to see what can be set
@@ -60,6 +66,13 @@ import systems.coyote.WebServer;
  */
 @Auth
 public class LogHandler extends AbstractJsonHandler implements UriResponder {
+  private static final String HDR_LOG_SIZE = "X-Log-Size";
+  private static final String OFFSET = "offset";
+  private static final long DEFAULT_OFFSET_FROM_END = 1024;
+  private long offset = 0;
+
+
+
 
   /**
    * 
@@ -70,9 +83,8 @@ public class LogHandler extends AbstractJsonHandler implements UriResponder {
     Config config = uriResource.initParameter( 1, Config.class );
     setFormattingJson( true );
 
-    // Get the command from the URL parameters specified when we were registered with the router 
+    // Get the name of the logger from the URL parameters specified when we were registered with the router 
     String name = urlParams.get( "name" );
-    String type = urlParams.get( "type" );
 
     if ( StringUtil.isBlank( name ) ) {
       DataFrame loggers = new DataFrame();
@@ -84,25 +96,84 @@ public class LogHandler extends AbstractJsonHandler implements UriResponder {
       results.add( "Loggers", loggers );
     } else {
       Logger logger = LogTool.getLogger( name );
-      if( logger != null ){
-      if ( "html".equalsIgnoreCase( type ) ) {
-
+      if ( logger != null ) {
+        String position = session.getParms().get( OFFSET );
+        if ( StringUtil.isNotBlank( position ) ) {
+          try {
+            offset = Long.parseLong( position );
+          } catch ( NumberFormatException e ) {
+            results.set( "error", "offset is not a valid number" );
+            setStatus( Status.BAD_REQUEST );
+          }
+        }
+        return textResponse( session, logger, offset );
       } else {
-
-      }
-      } else {
-        setStatus(Status.NOT_FOUND);
+        results.set( "error", "Logger not found with that name" );
+        setStatus( Status.NOT_FOUND );
       }
     }
 
-    return Response.createFixedLengthResponse( super.getStatus(), super.getMimeType(), super.getText() );
+    return Response.createFixedLengthResponse( getStatus(), getMimeType(), getText() );
   }
 
+
+
+
   /**
-   * @param status the status to set in this handler
+   * @param session the session requesting the response
+   * @param logger the logger requested
+   * @param offset number of bytes to skip before returning the log data
+   * @return
    */
-  private void setStatus( IStatus status ) {
-    super.st
+  private Response textResponse( IHTTPSession session, Logger logger, long offset ) {
+    File log = null;
+    URI target = logger.getTarget();
+    if ( UriUtil.isFile( target ) ) {
+      log = UriUtil.getFile( target );
+
+      if ( log.exists() ) {
+        if ( log.isDirectory() ) {
+          results.set( "error", "File '" + log + "' exists but is a directory" );
+          setStatus( Status.INTERNAL_ERROR );
+          return Response.createFixedLengthResponse( getStatus(), getMimeType(), getText() );
+        }
+        if ( log.canRead() == false ) {
+          results.set( "error", "File '" + log + "' cannot be read" );
+          setStatus( Status.INTERNAL_ERROR );
+          return Response.createFixedLengthResponse( getStatus(), getMimeType(), getText() );
+        }
+      } else {
+        results.set( "error", "File '" + log + "' does not exist" );
+        setStatus( Status.INTERNAL_ERROR );
+        return Response.createFixedLengthResponse( getStatus(), getMimeType(), getText() );
+      }
+
+      long size = log.length();
+      session.getResponseHeaders().put( HDR_LOG_SIZE, Long.toString( size ) );
+
+      // sanity checks on offset
+      if ( offset >= size ) {
+        offset = size - DEFAULT_OFFSET_FROM_END;
+      }
+      if ( offset < 0 ) {
+        offset = 0;
+      }
+
+      try {
+        InputStream fis = new FileInputStream( log );
+        fis.skip( offset );
+        return Response.createChunkedResponse( Status.OK, MimeType.TEXT.getType(), fis );
+      } catch ( Exception e ) {
+        Log.append( HTTPD.EVENT, getClass().getSimpleName() + " error sending '" + log.getAbsolutePath() + "' - " + e.getMessage() );
+        results.set( "error", "Problems processing '" + log + "' - " + e.getMessage() );
+        setStatus( Status.INTERNAL_ERROR );
+        return Response.createFixedLengthResponse( getStatus(), getMimeType(), getText() );
+      }
+    } else {
+      results.set( "error", "Logger target '" + target.toASCIIString() + "' is not a file" );
+      setStatus( Status.BAD_REQUEST );
+      return Response.createFixedLengthResponse( getStatus(), getMimeType(), getText() );
+    }
   }
 
 }
